bd2601669d5bf6d0606c4b2d58d588671263cb11
Владислав Лебедев <vlad-057@mail.ru>
2023-02-18 20:28:36 +0600

    finished basic functional

--- DigitalAssistant.js
+++ DigitalAssistant.js
@@ -3,7 +3,7 @@
 const PUBLIC_KEY = "BJa3I1_xBoM-ClXcJovm2Xyeo3EvdCP61ANrvXE0_Beey4yFllJ4VZ1eopBHL0DbwMWDNGg5XWAyY4BFTlpTDD4"
 const PRIVATE_KEY = "GN1_gOI8PrZtx3hr4P_fCTSNywBlvabSdzCCoLWrM_E"
 
-console.log("PUBLIC_KEY:", PUBLIC_KEY);
+var assistant = null;
 
 const defaults = {
     skills: [],
@@ -13,28 +13,41 @@ const defaults = {
 };
 
 const skills = [
+    {        
+        resolve: () => new Promise((resolve) => {
+            resolve("Привет, я цифровой помощник!");
+        }),
+        trigger: function(sen){
+            return sen.toLowerCase().includes("привет");
+        },                
+    },
+    {        
+        resolve: () => new Promise((resolve) => {
+            resolve("Для начала тебе нужно создать проект. Для этого для начала нажми на кнопку 'Мои Проекты'");
+        }),
+        trigger: function(sen){
+            var pageTitle = document.getElementsByTagName("title")[0].textContent;
+            if(pageTitle.toLocaleLowerCase().includes("главная")) return true;
+            return false;
+        }        
+    },
     {
-        "Hi": {
-            resolve: () => new Promise((resolve) => {
-                resolve("Привет, я цифровой помощник!");
-            }),
-            trigger: function(){
-                return sen.toLowerCase().includes("привет");
-            }
+        resolve: () => new Promise((resolve) => {
+            resolve("Ваши заявки находятся во вкладке Мои заявки слева");
+        }),
+        trigger: function(sen){
+            return (sen.toLowerCase().includes("где") && sen.toLowerCase().includes("заявк"));
         }
     },
     {
-        "NavigateStep1": {
-            resolve: () => new Promise((resolve) => {
-                resolve("Для начала тебе нужно создать проект. Для этого для начала нажми на кнопку 'Мои Проекты'");
-            }),
-            trigger: function(){
-                var pageTitle = document.getElementsByTagName("title")[0].textContent;
-                if(pageTitle.toLocaleLowerCase().includes("главная")) return true;
-                return false;
-            }
+        resolve: () => new Promise((resolve) => {
+            resolve("Ваши проекты находятся во вкладке Мои проекты слева");
+        }),
+        trigger: function(sen){
+            return (sen.toLowerCase().includes("где") && sen.toLowerCase().includes("проект"));
         }
-    }
+    },
+      
 ]
 
 const chatComponentId = "chat";
@@ -51,58 +64,63 @@ class DigitalAssistant {
         }
 
         this.config = { ...defaults, ...config };
-        
+
         // распознавание голоса
-        this.recognizer = new SpeechRecognition();
-        this.recognizer.lang = this.config.language;
-        this.recognizer.addEventListener('end', () => {
-        this.recognizer.start();
-        });
-        this.recognizer.addEventListener('result', (instance) => {
-        const transcript = Array.from(instance.results)
-            .map(result => result[0])
-            .map(result => result.transcript)
-            .map(str => str.toLowerCase())[0];
-        const words = transcript.split(' ');
-        if (words[0] === this.config.name.toLocaleLowerCase()) {
-            this.process(words.slice(1).join(' '));
-        }
-        });
+
+        
+
+        // this.recognizer = new webkitSpeechGrammar();
+        // this.recognizer.lang = this.config.language;
+        // this.recognizer.addEventListener('end', () => {
+        // this.recognizer.start();
+        // });
+        // this.recognizer.addEventListener('result', (instance) => {
+        //     const transcript = Array.from(instance.results)
+        //         .map(result => result[0])
+        //         .map(result => result.transcript)
+        //         .map(str => str.toLowerCase())[0];
+        //     const words = transcript.split(' ');
+        //     if (words[0] === this.config.name.toLocaleLowerCase()) {
+        //         this.process(words.slice(1).join(' '));
+        //     }
+        // });
     }
     start() {
-        this.recognizer.start();
+        //this.recognizer.start();
         
         if ('PushManager' in window) { 
             Notification.requestPermission();
         }
         
         // добавление html помощника на страницу
-        var html = readTextFile(this.assistantFile); 
-        var div = document.createElement('div');
-        div.innerHTML = html.trim();
-        document.getElementsByTagName("body")[0].append(div);
+        // var html = readTextFile(this.assistantFile); 
+        // var div = document.createElement('div');
+        // div.innerHTML = html.trim();
+        // document.getElementsByTagName("body")[0].append(div);
     }
     process(sentence) {
+        console.log(this.config.skills);
         for (const skill of this.config.skills)
-        if (skill.trigger(sentence))
-            return skill.resolve(sentence).then(s => this.say(s));
+            if (skill.trigger(sentence))
+                return skill.resolve(sentence).then(s => this.say(s));
         return this.say(this.config.reply);
     }
 
-    addMessageToChat(message){
+    addMessageToChat(message, who){
         var chatElement = document.getElementById(chatComponentId);   
-        messageElement = document.createElement("div");
-        messageElement.innerHTML = ```
-        html сообщения
-        
-        ```
+        var messageElement = document.createElement("div");
+        messageElement.innerHTML = `
+            <a class="btn btn-teal message ${who}">
+                ${message}
+            </a>        
+        `
         chatElement.appendChild(messageElement);
     }
 
     say(sentence) {
         const filtered = sentence.replace(/[&\/\\#,+()$~%.'"*?<>{}]/g, '');
         const msg = new SpeechSynthesisUtterance(filtered);
-        addMessageToChat(msg);
+        this.addMessageToChat(msg.text, "bot");
         window.speechSynthesis.speak(msg);
     }
 
@@ -118,11 +136,28 @@ class DigitalAssistant {
 }
 
 document.addEventListener('DOMContentLoaded', function(){
-    const assistant = new DigitalAssistant({
+    assistant = new DigitalAssistant({
         name: "Oleg",
         skills: skills,        
     })
 
     assistant.start();
     assistant.process("Привет");
+
+    sendButton = document.getElementById("send");
+    botInput = document.getElementById("bot_input");
+    sendButton.addEventListener('click', function() { 
+        assistant.addMessageToChat(botInput.value, "me")
+        assistant.process(botInput.value);
+        botInput.value  = "";
+    }, false);
+
+    botInput.onkeypress = function(e){
+        if (!e) e = window.event;
+        var keyCode = e.code || e.key;
+        if (keyCode == 'Enter'){
+          sendButton.click();
+          return false;
+        }
+    }
 });
